d28eadd397cb32a7626a23d4ad0eb129c19ae72f
you <ydd@tyll.net.cn>
2022-01-27 14:39:36 +0800

    ydd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e95a2d1d18bcd201523739967e946e9dbad52e7e',
+    'name' => 'youdongdong/tool',
+  ),
+  'versions' => 
+  array (
+    'youdongdong/tool' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e95a2d1d18bcd201523739967e946e9dbad52e7e',
+    ),
+  ),
+);
